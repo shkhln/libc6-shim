db6a74802fc8086cb4347c49a4d135b8d923f3c9
Alex S <shkhln@users.noreply.github.com>
2018-11-13 02:20:39 +0300

    Fix UVM stub.

--- src/libc/sys/ioctl.c
+++ src/libc/sys/ioctl.c
@@ -1,19 +1,30 @@
 #include <assert.h>
 #include <stdarg.h>
 #include <stdio.h>
+#include <stdint.h>
 #include <sys/ioctl.h>
 #include "../../shim.h"
 
 #define NV_UVM_INITIALIZE   0x30000001
 #define NV_UVM_DEINITIALIZE 0x30000002
 
+#define NV_ERR_NOT_SUPPORTED 0x56
+
 #define NV_ESC_SYS_PARAMS 214
 #define NV_ESC_NUMA_INFO  215
+
+struct NvUvmInitParams
+{
+  uint64_t flags __attribute__((aligned(8)));
+  uint32_t status;
+};
 
 int shim_ioctl_impl(int fd, unsigned long request, va_list args) {
 
   if (request == NV_UVM_INITIALIZE) {
-    return -1;
+    struct NvUvmInitParams* params = (struct NvUvmInitParams*)va_arg(args, void*);
+    params->status = NV_ERR_NOT_SUPPORTED;
+    return 0;
   }
 
   if (request == NV_UVM_DEINITIALIZE) {
