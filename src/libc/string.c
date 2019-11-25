5ae3e224efb56a50e635ede0d825c35f27077796
Alex S <shkhln@users.noreply.github.com>
2019-11-26 00:51:10 +0300

    Add strerror_r.

--- src/libc/string.c
+++ src/libc/string.c
@@ -5,3 +5,8 @@
 void* shim___rawmemchr_impl(const void* s, int c) {
   return memchr(s, c, INT_MAX);
 }
+
+char* shim_strerror_r_impl(int errnum, char* strerrbuf, size_t buflen) {
+  strerror_r(errnum, strerrbuf, buflen);
+  return strerrbuf;
+}
