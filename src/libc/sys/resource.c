0eeb57683c1758e0c4d97b6faa30b28238e40c62
Alex S <shkhln@users.noreply.github.com>
2020-03-19 16:10:50 +0300

    Stub rlimit.

--- src/libc/sys/resource.c
+++ src/libc/sys/resource.c
@@ -1,10 +1,17 @@
+#include <errno.h>
 #include <sys/types.h>
 #include <sys/time.h>
 #include <sys/resource.h>
 #include "../../shim.h"
 
+int shim_getrlimit_impl(int resource, linux_rlimit* rlp) {
+  errno = native_to_linux_errno(EPERM);
+  return -1;
+}
+
 int shim_setrlimit_impl(int resource, const linux_rlimit* rlp) {
   UNIMPLEMENTED();
 }
 
+SHIM_WRAP(getrlimit);
 SHIM_WRAP(setrlimit);
