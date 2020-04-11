c2389363821a5435054aa1af5ec9a74ccdcdf19f
Alex S <shkhln@users.noreply.github.com>
2020-04-11 07:17:50 +0300

    Add 440.82 to the setup script.

--- utils/set_up_libgl.rb
+++ utils/set_up_libgl.rb
@@ -13,7 +13,8 @@ SHA256 = {
   'NVIDIA-Linux-x86_64-440.26.run': '5f98e7af0643aea8c5538128bfef2f70dab0cd2559c97d0a8598210a3761c32b',
   'NVIDIA-Linux-x86_64-440.31.run': 'cd592f385c9bfb798f973225dfd086654973984762b669c7d765b074c0d8850f',
   'NVIDIA-Linux-x86_64-440.59.run': '61cb1a6c1017087d431c8cec4fc0450030d469cb825a4cac7d1a5142b8c14f98',
-  'NVIDIA-Linux-x86_64-440.64.run': 'e13c9f1ba13262bb7a9c4bb8180f7beb09be2b17145f611a81e8cf92600b7575'
+  'NVIDIA-Linux-x86_64-440.64.run': 'e13c9f1ba13262bb7a9c4bb8180f7beb09be2b17145f611a81e8cf92600b7575',
+  'NVIDIA-Linux-x86_64-440.82.run': 'edd415acf2f75a659e0f3b4f27c1fab770cf21614e84a18152d94f0d004a758e'
 }
 
 fetch_dir = __dir__ + '/../nvidia'
@@ -86,12 +87,13 @@ if driver_version.split('.').first.to_i >= 410
   libs64 << 'libnvidia-fatbinaryloader.so.' + driver_version
   libs64 << 'libnvidia-ptxjitcompiler.so.'  + driver_version
   libs64 << 'libnvidia-rtcore.so.'          + driver_version
-  libs64 << 'libcuda.so.'                   + driver_version
-  libs64 << 'libnvcuvid.so.'                + driver_version
-  libs64 << 'libnvidia-compiler.so.'        + driver_version
-  libs64 << 'libnvidia-encode.so.'          + driver_version
-  libs64 << 'libnvidia-opencl.so.'          + driver_version
 end
+
+libs64 << 'libcuda.so.'            + driver_version
+libs64 << 'libnvcuvid.so.'         + driver_version
+libs64 << 'libnvidia-compiler.so.' + driver_version
+libs64 << 'libnvidia-encode.so.'   + driver_version
+libs64 << 'libnvidia-opencl.so.'   + driver_version
 
 system("tail -n +#{skip} #{installer} | xz -d | tar -C #{lib64_dir} -xf -                      #{libs64.join(' ')}")
 system("tail -n +#{skip} #{installer} | xz -d | tar -C #{lib32_dir} -xf - --strip-components 2 #{libs32.join(' ')}")
