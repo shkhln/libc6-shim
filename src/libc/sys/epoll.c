f944da5ce3e343aa6eb91c349a54bf6f68665067
Alex S <shkhln@users.noreply.github.com>
2020-08-09 14:19:09 +0300

    Stub epoll.

---
+++ src/libc/sys/epoll.c
@@ -0,0 +1,30 @@
+#include <unistd.h>
+#include "../../shim.h"
+#include "fcntl.h"
+
+static int fake_epoll_fd = -1;
+
+__attribute__((constructor))
+static void init_fake_epoll_fd() {
+
+  int inout[2];
+  {
+    int err = pipe(inout);
+    assert(err == 0);
+  }
+
+  fake_epoll_fd = inout[1];
+}
+
+int shim_epoll_create_impl(int size) {
+  return fake_epoll_fd;
+}
+
+typedef void linux_epoll_event;
+
+int shim_epoll_ctl_impl(int epfd, int op, int fd, linux_epoll_event* event) {
+  return 0; // ?
+}
+
+SHIM_WRAP(epoll_create);
+SHIM_WRAP(epoll_ctl);
