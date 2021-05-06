be049c9e2804da8b592de1fca0865b822c40f79f
Alex S <shkhln@users.noreply.github.com>
2021-05-06 19:46:20 +0300

    Stub epoll_wait.

--- src/libc/sys/epoll.c
+++ src/libc/sys/epoll.c
@@ -26,5 +26,10 @@ int shim_epoll_ctl_impl(int epfd, int op, int fd, linux_epoll_event* event) {
   return 0; // ?
 }
 
+int shim_epoll_wait_impl(int epfd, linux_epoll_event*events, int maxevents, int timeout) {
+  return -1; // ?
+}
+
 SHIM_WRAP(epoll_create);
 SHIM_WRAP(epoll_ctl);
+SHIM_WRAP(epoll_wait);
