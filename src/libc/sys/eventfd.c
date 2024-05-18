446aa75009ef0e472654670eada954e81748e3e2
Alex S <shkhln@users.noreply.github.com>
2024-05-18 23:46:54 +0300

    Use real eventfd.

--- src/libc/sys/eventfd.c
+++ src/libc/sys/eventfd.c
@@ -1,11 +1,11 @@
 #include <unistd.h>
+#include <sys/eventfd.h>
 #include "../../shim.h"
 #include "fcntl.h"
 
 #define LINUX_EFD_NONBLOCK 0x00800
 #define LINUX_EFD_CLOEXEC  0x80000
 
-//TODO: wrap native eventfd
 int shim_eventfd_impl(unsigned int initval, int linux_flags) {
 
   assert((linux_flags & ~(LINUX_EFD_CLOEXEC | LINUX_EFD_NONBLOCK)) == 0);
@@ -14,13 +14,7 @@ int shim_eventfd_impl(unsigned int initval, int linux_flags) {
   if (linux_flags & LINUX_EFD_NONBLOCK) flags |= O_NONBLOCK;
   if (linux_flags & LINUX_EFD_CLOEXEC)  flags |= O_CLOEXEC;
 
-  int inout[2];
-  {
-    int err = pipe2(inout, flags);
-    assert(err == 0);
-  }
-
-  return inout[1];
+  return eventfd(initval, flags);
 }
 
 SHIM_WRAP(eventfd);
