1c98c1d14de755eff49758afbcfeedae7e8021aa
Alex S <shkhln@users.noreply.github.com>
2025-02-14 23:57:24 +0300

    Stub ftw64 and nftw64.

--- src/libc/ftw.c
+++ src/libc/ftw.c
@@ -7,9 +7,19 @@ static int shim_ftw_impl(const char* path, int (*fn)(const char*, const struct l
   return -1;
 }
 
+static int shim_ftw64_impl(const char* path, int (*fn)(const char*, const linux_stat64*, int), int maxfds) {
+  return -1;
+}
+
 static int shim_nftw_impl(const char* path, int (*fn)(const char*, const struct linux_stat*, int, linux_FTW*), int maxfds, int flags) {
   return -1;
 }
 
+static int shim_nftw64_impl(const char* path, int (*fn)(const char*, const linux_stat64*, int, linux_FTW*), int maxfds, int flags) {
+  return -1;
+}
+
 SHIM_WRAP(ftw);
+SHIM_WRAP(ftw64);
 SHIM_WRAP(nftw);
+SHIM_WRAP(nftw64);
