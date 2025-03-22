a5498635023bedec3a9a16b5b943b0325e31c98e
Alex S <shkhln@users.noreply.github.com>
2025-03-22 20:46:13 +0300

    Add EFD_SEMAPHORE.

--- src/libc/sys/eventfd.c
+++ src/libc/sys/eventfd.c
@@ -3,16 +3,18 @@
 #include "../../shim.h"
 #include "fcntl.h"
 
-#define LINUX_EFD_NONBLOCK 0x00800
-#define LINUX_EFD_CLOEXEC  0x80000
+#define LINUX_EFD_SEMAPHORE       1
+#define LINUX_EFD_NONBLOCK  0x00800
+#define LINUX_EFD_CLOEXEC   0x80000
 
 static int shim_eventfd_impl(unsigned int initval, int linux_flags) {
 
-  assert((linux_flags & ~(LINUX_EFD_CLOEXEC | LINUX_EFD_NONBLOCK)) == 0);
+  assert((linux_flags & ~(LINUX_EFD_CLOEXEC | LINUX_EFD_NONBLOCK | LINUX_EFD_SEMAPHORE)) == 0);
 
   int flags = 0;
-  if (linux_flags & LINUX_EFD_NONBLOCK) flags |= EFD_NONBLOCK;
-  if (linux_flags & LINUX_EFD_CLOEXEC)  flags |= EFD_CLOEXEC;
+  if (linux_flags & LINUX_EFD_SEMAPHORE) flags |= EFD_SEMAPHORE;
+  if (linux_flags & LINUX_EFD_NONBLOCK)  flags |= EFD_NONBLOCK;
+  if (linux_flags & LINUX_EFD_CLOEXEC)   flags |= EFD_CLOEXEC;
 
   return eventfd(initval, flags);
 }
